36a5940d6f5777772e8223286672c2f1526eada1
kosuke <sasaki.kosuke8@gmail.com>
2025-09-09 10:33:46 +0900

    ヘッダー固定修正

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -6,40 +6,42 @@ import styles from "./index.module.scss";
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <Link href="/">
-        <Image
-          src={`${process.env.NEXT_PUBLIC_BASE_PATH}/logo_vein.png`}
-          alt="Vein Logo"
-          width={151}
-          height={49}
-          priority
-        />
-      </Link>
-      <nav>
-        <Link href="/">HOME</Link>
-        <a
-          href="https://reserva.be/vein"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          BOOKING
-        </a>
-        <Link href="/info">INFO</Link>
-        <Link href="/contact">CONTACT</Link>
-        <a
-          href="https://www.instagram.com/vein__relaxation"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <div className={styles.headerInner}>
+        <Link href="/">
           <Image
-            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/instagram_white.svg`}
-            alt="instagram"
-            width={20}
-            height={20}
+            src={`${process.env.NEXT_PUBLIC_BASE_PATH}/logo_vein.png`}
+            alt="Vein Logo"
+            width={151}
+            height={49}
             priority
           />
-        </a>
-      </nav>
+        </Link>
+        <nav>
+          <Link href="/">HOME</Link>
+          <a
+            href="https://reserva.be/vein"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BOOKING
+          </a>
+          <Link href="/info">INFO</Link>
+          <Link href="/contact">CONTACT</Link>
+          <a
+            href="https://www.instagram.com/vein__relaxation"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={`${process.env.NEXT_PUBLIC_BASE_PATH}/instagram_white.svg`}
+              alt="instagram"
+              width={20}
+              height={20}
+              priority
+            />
+          </a>
+        </nav>
+      </div>
     </header>
   );
 };
